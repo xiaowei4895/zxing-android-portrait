41d92e87aa9af515b09d2e33c1465a21bae8cfdb
xiaowei4895 <xiaowei4895@163.com>
2014-05-18 20:26:03 +0800

    Change scan area to square.

--- src/com/google/zxing/client/android/camera/CameraManager.java
+++ src/com/google/zxing/client/android/camera/CameraManager.java
@@ -214,9 +214,12 @@ public final class CameraManager {
         // Called early, before init even finished
         return null;
       }
-
-      int width = findDesiredDimensionInRange(screenResolution.x, MIN_FRAME_WIDTH, MAX_FRAME_WIDTH);
-      int height = findDesiredDimensionInRange(screenResolution.y, MIN_FRAME_HEIGHT, MAX_FRAME_HEIGHT);
+      
+      int resolutionMin = Math.min(screenResolution.x, screenResolution.y);
+      int width = findDesiredDimensionInRange(resolutionMin,MIN_FRAME_HEIGHT, MAX_FRAME_HEIGHT);
+      int height = width;
+//      int width = findDesiredDimensionInRange(screenResolution.x, MIN_FRAME_WIDTH, MAX_FRAME_WIDTH);
+//      int height = findDesiredDimensionInRange(screenResolution.y, MIN_FRAME_HEIGHT, MAX_FRAME_HEIGHT);
 
       int leftOffset = (screenResolution.x - width) / 2;
       int topOffset = (screenResolution.y - height) / 2;
